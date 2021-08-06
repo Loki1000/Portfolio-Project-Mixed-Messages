6fb3b8bd328383984ac0b4b90ebf861832949b5f
Loki1000 <138sharks@gmail.com>
2021-08-05 18:05:04 -0700

    Forgot to add ;s

--- Mixed Messages.js
+++ Mixed Messages.js
@@ -1,7 +1,7 @@
-let partOne = ["I ", "I am ", "Don't make me ", "Hulk ", "Who's asking? "]
-let partTwo = ["am ", "burdened with ", "hungry ", "smash ", "I know you're asking. "]
-let partThree = ["Iron Man", "glorius purpose", "you won't like me when I'm hungry", "!!!", "Is anyone else asking, or is it just you?"]
-let random1 = Math.floor(Math.randon() * 5)
-let random2 = Math.floor(Math.randon() * 5)
-let random3 = Math.floor(Math.randon() * 5)
-console.log(partOne[random1] + partTwo[random2] + partThree[random3])
+let partOne = ["I ", "I am ", "Don't make me ", "Hulk ", "Who's asking? "];
+let partTwo = ["am ", "burdened with ", "hungry ", "smash ", "I know you're asking. "];
+let partThree = ["Iron Man", "glorius purpose", "you won't like me when I'm hungry", "!!!", "Is anyone else asking, or is it just you?"];
+let random1 = Math.floor(Math.randon() * 5);
+let random2 = Math.floor(Math.randon() * 5);
+let random3 = Math.floor(Math.randon() * 5);
+console.log(partOne[random1] + partTwo[random2] + partThree[random3]);
